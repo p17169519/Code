47cddb30c37c5e920e4f92ef72dd768fd2ccfe67
Chris Freeman <p17169519@my365.dmu.ac.uk>
2021-03-27 09:55:35 +0000

    First commit for FYP index.html menu.html login.html

---
+++ javaScript/scripts.js
@@ -0,0 +1,7 @@
+
+topbutton = document.getElementById("topBtn");
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
