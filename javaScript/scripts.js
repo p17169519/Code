655d336e430d688c3404713d8dc171a14a4fd4fa
Chris Freeman <p17169519@my365.dmu.ac.uk>
2021-03-27 13:02:06 +0000

    worked on basket menu

--- javaScript/scripts.js
+++ javaScript/scripts.js
@@ -5,3 +5,8 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+menuToggler.addEventListener('click', ev => {
+  myBasket.classList.toggle('open');
+  menuToggler.textContent = menuToggler.textContent === "x" ? "≡" : "x";
+});
